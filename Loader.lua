48ad9ed44be68d4e4250f49d9d1c20681be424fa
DevouredXd <mccjhon9@gmail.com>
2026-04-10 01:47:24 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
+local base = "https://raw.githubusercontent.com/DevouredXd/NeverLoseV6/main/"
 
+-- will load key system
+loadstring(game:HttpGet(base .. "keysystem.lua"))()
+
+-- will load main ui and script hopefully.
+loadstring(game:HttpGet(base .. "main.lua"))()
